f8d0a7b084711cc9e424550286f42e6d377b55a5
johanbasi23 <johanbasilio23@gmail.com>
2026-02-28 19:50:20 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import edgeoneAdapter from '@edgeone/astro';
 
-// https://astro.build/config
+// 
 export default defineConfig({
+  site: 'https//johanbasi23.github.io',
   adapter: edgeoneAdapter(),
   output: 'static',
   integrations: [react()]
