1f932e033ecbc25402230c693d1ca728928b38c7
johanbasi23 <johanbasilio23@gmail.com>
2026-02-28 21:06:48 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import edgeoneAdapter from '@edgeone/astro';
 
-// 
 export default defineConfig({
   site: 'https://johanbasi23.github.io',
-  adapter: edgeoneAdapter(),
+  // Si tu repo NO se llama johanbasi23.github.io sino algo como "mi-portfolio":
+  // base: '/nombre-del-repo',
   output: 'static',
   integrations: [react()]
 });
-
-
